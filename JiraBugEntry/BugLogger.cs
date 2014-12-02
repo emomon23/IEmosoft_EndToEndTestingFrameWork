4bac9a35c4dc44b35d8645bf56ffe81d9840176c
emomon23 <memo@iemosoft.com>
2014-12-02 06:01:18 -0600

    Addded 'AnotherJiraRestClient' dll to JiraBugEntry lib

--- JiraBugEntry/BugLogger.cs
+++ JiraBugEntry/BugLogger.cs
@@ -7,24 +7,19 @@ using System.Diagnostics;
 using System.Windows.Forms;
 using RecordableBrowser;
 using RecordableBrowser.Interfaces;
-using OpenQA;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Firefox;
-using OpenQA.Selenium.Support;
+using AnotherJiraRestClient;
+using AnotherJiraRestClient.JiraModel;
 
 namespace JiraBugEntry
 {
     public class BugLogger : BugCreator
     {
-        private string loginURL = "https://www.lokitali.com/loki_jira/login.jsp";
-        private IWebDriver browser;
-       
+        JiraClient client = null;
+
         public override void CreateBug(TestRecorderModel.TestCaseData header, List<TestRecorderModel.TestCaseStep> steps)
         {
-            browser = new FirefoxDriver();
             base.InitializeBugCreator(header, steps);
-
-            this.Login();
+            this.InitializeJiraClient();
 
             if (this.BugPreviouslyEntered())
                 return;
@@ -32,46 +27,40 @@ namespace JiraBugEntry
             this.CreateNewBug();
        }
 
+        private void InitializeJiraClient()
+        {
+            if (client == null)
+            {
+                //Replace these 4 lines of code with your Jira creds and url
+                JiraInstance credentialsReader = new JiraInstance();
+                string userName = credentialsReader.Username;
+                string password = credentialsReader.Password;
+                string url = credentialsReader.Url;
+
+                JiraAccount account = new JiraAccount()
+                                        {
+                                            Password = password,
+                                            ServerUrl = url ,
+                                            User = userName
+                                        };
+
+                client = new JiraClient(account);
+            }
+        }
+
         private void CreateNewBug()
         {
-            browser.ClickElement(By.Id(LokiControls.DashboardPage.CreateBugLinkId));
-
-            browser.SetTextOnElement(LokiControls.CreateIssuePage.SummaryId, BugTitle);
-            browser.SetTextOnElement(LokiControls.CreateIssuePage.DueDateId, DateTime.Now.AddDays(7).ToShortDateString());
-            browser.SetTextOnElement(LokiControls.CreateIssuePage.DescriptionId, BugDescription);
-
-            if (base.ImageFileExists)
-            {
-                browser.ClickElement(By.Id(LokiControls.CreateIssuePage.AttachementChooseFileButtonId));
-                System.Threading.Thread.Sleep(3000);
-                SendKeys.Send(base.PathToFailedImage + "{ENTER}");
-                System.Threading.Thread.Sleep(3000);
-            }
-
-            browser.ClickElement(By.Id(LokiControls.CreateIssuePage.SubmitButtonId));
+           
         }
-
-        private void Login()
-        {
-            browser.Navigate().GoToUrl(loginURL);
-            browser.SetTextOnElement(LokiControls.LoginPage.UserNameId, "memo");
-            browser.SetTextOnElement(LokiControls.LoginPage.PasswordId, "NotReallyMyPassword");
-            browser.ClickElement(By.Id(LokiControls.LoginPage.LoginSubmitButtonId));
-        }
-
+        
         private bool BugPreviouslyEntered()
         {
-            if (!browser.PageContains("System Dashboard"))
-                return true;  //We're not on the dashboard, don't try to proceed, just say thie bug already exists and end it there
-
-            string subsetDescription = this.BugDescription.Substring(0, 25);
-
-            return browser.PageContains(BugTitle) && browser.PageContains(subsetDescription);
+            return false;
         }
         
         public override void Dispose()
         {
-            browser.Quit();
+          
         }
     }
 }
---
+++ JiraBugEntry/JiraInstance.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace JiraBugEntry
+{
+    public class JiraInstance
+    {
+        public JiraInstance()
+        {
+            string fileName = @"C:\Projects\Data.dat";
+            string tagName = "LokiJira";
+
+            string[] filelines = File.ReadAllLines(fileName);
+            bool gotIt = false;
+
+            foreach (string line in filelines)
+            {
+                if (line.Contains(tagName))
+                {
+                    gotIt = true;
+                    string [] values = line.Split(',');
+
+                    this.Username = values[1];
+                    this.Password = values[2];
+                    this.Url = values[3];
+                    break;
+                }
+            }
+
+            if (!gotIt)
+            {
+                throw new Exception(string.Format("Unable to '{0}' in file {1}", tagName, fileName));
+            }
+        }
+
+        public string Url { get; private set; }
+        public string Username { get; private set; }
+        public string Password { get; private set; }
+    }
+}
