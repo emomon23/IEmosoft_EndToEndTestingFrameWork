a6a82477f833e66e94cb98972a3c871e5f067fcf
Mike Emo <MEmo@mpifix.com>
2016-03-22 15:07:27 -0500

    RegisterTestUnderDevelopment now supports asserting on behave of the superclass

--- iEmosoft_TestExecutioner/BaseClasses/BaseTestClass.cs
+++ iEmosoft_TestExecutioner/BaseClasses/BaseTestClass.cs
@@ -5,12 +5,20 @@ using System.Text;
 using System.Threading.Tasks;
 using iEmosoft.Automation.Test.IEmosoft.com;
 using iEmosoft.Automation.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace iEmosoft.Automation.BaseClasses
 {
     public abstract class BaseTestClass
     {
-        protected void RegisterTestUnderDevelopment(string testNumber, string testName, string testDescription, string testFamily, string etaDate)
+        public enum RegisterTestUnderDevelopmentResultEnumeration
+        {
+            AssertInconclusive,
+            AssertFailure,
+            DoNothing
+        }
+
+        protected void RegisterTestUnderDevelopment(string testNumber, string testName, string testDescription, string testFamily, string etaDate, RegisterTestUnderDevelopmentResultEnumeration assertAction = RegisterTestUnderDevelopmentResultEnumeration.AssertInconclusive)
         {
             RestClient restClient = new RestClient();
 
@@ -26,6 +34,16 @@ namespace iEmosoft.Automation.BaseClasses
                 eta = temp;
             }
             restClient.RegisterTest(testNumber, testFamily, testName, testDescription, eta);
+
+            string assertMsg = "This test is has yet to be implemented.";
+            if (assertAction == RegisterTestUnderDevelopmentResultEnumeration.AssertInconclusive)
+            {
+                Assert.Inconclusive(assertMsg);
+            }
+            else if (assertAction == RegisterTestUnderDevelopmentResultEnumeration.AssertFailure)
+            {
+                Assert.IsTrue(false, assertMsg);
+            }
         }
 
         protected void RegisterTestUnderDevelopment(TestCaseHeaderData headerData, string etaDate)
